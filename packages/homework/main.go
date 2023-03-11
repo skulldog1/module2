504b05db0c7856f67387a5a2880e4b514a427c89
skulldog1 <ehd1996@mail.ru>
2023-03-11 20:46:26 +0300

    Create main.go

---
+++ packages/homework/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ptflp/greet"
+)
+
+func main() {
+	greet.Hello()
+
+	fmt.Println(greet.Shark)
+
+	oct := greet.Octopus{
+		Name:  "Jesse",
+		Color: "orange",
+	}
+
+	fmt.Println(oct.String())
+
+	oct.Reset()
+
+	fmt.Println(oct.String())
+}
